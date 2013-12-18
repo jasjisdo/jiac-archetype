79ea871e7e5fcfcb8e863257258920f865467880
Jaschar Domann <Jaschar.Domann@dai-labor.de>
2013-12-18 19:37:53 +0100

    Initial commit

---
+++ jiac-jsw/src/main/resources/archetype-resources/src/main/java/__MainClass__.java
@@ -0,0 +1,13 @@
+package ${package};
+
+import de.dailab.jiactng.agentcore.SimpleAgentNode;
+
+/**
+ * this is main class
+ */
+public class ${MainClass}{
+	
+	public static void main(String[] args) {
+		SimpleAgentNode.main(new String[]{"/node-config/node.xml"});
+	}
+}
---
+++ jiac-jsw/src/main/resources/archetype-resources/src/main/java/__MainClass__Bean.java
@@ -0,0 +1,34 @@
+package ${package};
+
+import de.dailab.jiactng.agentcore.AbstractAgentBean;
+
+public class ${MainClass}Bean extends AbstractAgentBean{
+
+	  /**
+	   * {@inheritDoc}
+	   */
+	  @Override
+	  public void doInit() throws Exception {
+	  }
+
+	  /**
+	   * {@inheritDoc}
+	   */
+	  @Override
+	  public void doStart() throws Exception {
+	  }
+
+	  /**
+	   * {@inheritDoc}
+	   */
+	  @Override
+	  public void doStop() throws Exception {
+	  }
+	  
+	  /**
+	   * {@inheritDoc}
+	   */
+	  public void execute() {
+		 //use sensors or (agent-)memory control here
+	  }
+}
