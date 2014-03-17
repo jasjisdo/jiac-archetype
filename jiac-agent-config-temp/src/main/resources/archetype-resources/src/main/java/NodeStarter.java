5d2d4c0477b91838b817fd4dcc6f79eaa54a3636
Jaschar Domann <Jaschar.Domann@dai-labor.de>
2014-03-17 11:49:53 +0100

    add new module used for evaluation in bachelor thesis

---
+++ jiac-agent-config-temp/src/main/resources/archetype-resources/src/main/java/NodeStarter.java
@@ -0,0 +1,13 @@
+package ${package};
+
+import de.dailab.jiactng.agentcore.SimpleAgentNode;
+
+public class NodeStarter {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+            SimpleAgentNode.main(new String[] { "classpath:AgentNode.xml" });
+    }
+}
